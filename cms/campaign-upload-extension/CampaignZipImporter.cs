d023374a29eb3de126dadcf0324fbe330f640ef7
Niclas Cedermalm <ncedermalm@sdl.com>
2017-09-10 21:28:17 +0200

    Added support for extracting of images and properties when uploading HTML campaign ZIP files

--- cms/campaign-upload-extension/CampaignZipImporter.cs
+++ cms/campaign-upload-extension/CampaignZipImporter.cs
@@ -11,6 +11,7 @@ using Tridion.ContentManager.Extensibility.Events;
 using Tridion.Logging;
 using Tridion.ContentManager.ContentManagement.Fields;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace SDL.Web.Extensions.CampaignUpload
 {
@@ -42,8 +43,13 @@ namespace SDL.Web.Extensions.CampaignUpload
             {
                 ItemFields content = new ItemFields(component.Metadata, component.MetadataSchema);
                 EmbeddedSchemaField taggedContentList = (EmbeddedSchemaField)content["taggedContent"];
+                EmbeddedSchemaField taggedImageList = (EmbeddedSchemaField)content["taggedImages"];
+                EmbeddedSchemaField taggedPropertyList = (EmbeddedSchemaField)content["taggedProperties"];
 
-                if ( taggedContentList.Values.Count > 0 )
+                var orgItem = component.OrganizationalItem;
+                var session = component.Session;
+
+                if ( taggedContentList.Values.Count > 0 || taggedImageList.Values.Count > 0 )
                 {
                     // Just do the extraction of content fields from the HTML the first time
                     //
@@ -68,44 +74,134 @@ namespace SDL.Web.Extensions.CampaignUpload
                     {
                         html = reader.ReadToEnd();
                     }
-                }
 
-                if (html != null)
-                {
-                    // Parse the HTML and find all content items
-                    //
-                    var htmlDoc = new HtmlDocument();
-                    htmlDoc.LoadHtml(html);
-                    htmlDoc.OptionOutputAsXml = true;
-           
-                    Schema taggedContentSchema = ( (EmbeddedSchemaFieldDefinition) taggedContentList.Definition).EmbeddedSchema;
-                 
-                    foreach ( var node in htmlDoc.DocumentNode.QuerySelectorAll("[data-content-name]") )
+                    if (html != null)
                     {
-                        // Add XHTML namespace to all elements in the content markup
+                        // Parse the HTML and find all content items
                         //
-                        foreach ( var element in node.QuerySelectorAll("*") )
+                        var htmlDoc = new HtmlDocument();
+                        htmlDoc.LoadHtml(html);
+                        htmlDoc.OptionOutputAsXml = true;
+
+                        Schema taggedContentSchema = ((EmbeddedSchemaFieldDefinition)taggedContentList.Definition).EmbeddedSchema;
+                        EmbeddedSchemaFieldDefinition taggedImageField = (EmbeddedSchemaFieldDefinition) taggedImageList.Definition;
+                        Schema taggedImageSchema = taggedImageField.EmbeddedSchema;
+                        SchemaFields taggedImageSchemaFields = new SchemaFields(taggedImageSchema);
+                        MultimediaLinkFieldDefinition mmLinkFieldDef = (MultimediaLinkFieldDefinition) taggedImageSchemaFields.Fields[1];
+                        Schema taggedPropertySchema= ((EmbeddedSchemaFieldDefinition)taggedPropertyList.Definition).EmbeddedSchema;
+                        
+                        Schema imageSchema = mmLinkFieldDef.AllowedTargetSchemas[0];
+
+                        if (imageSchema != null)
                         {
-                            element.SetAttributeValue("xmlns", "http://www.w3.org/1999/xhtml");
-                        } 
+                            Logger.Write("Image Schema: " + imageSchema, "CampaignZipImporter", LogCategory.Custom, System.Diagnostics.TraceEventType.Information);
+                        }
+                        //TODO: What to do if image schema is null??
 
-                        var taggedContentXml = new StringBuilder();
-                        taggedContentXml.Append("<TaggedContent><name>");
-                        taggedContentXml.Append(node.Attributes["data-content-name"].Value);
-                        taggedContentXml.Append("</name><content>");
-                 
-                        taggedContentXml.Append(node.InnerHtml);
-                        taggedContentXml.Append("</content></TaggedContent>");
-                        XmlDocument xmlDoc = new XmlDocument();
-                        xmlDoc.LoadXml(taggedContentXml.ToString());
-                        ItemFields taggedContent = new ItemFields(xmlDoc.DocumentElement, taggedContentSchema);
-                        taggedContentList.Values.Add(taggedContent);
+                        // TODO: Refactor into several methods here
+
+                        foreach (var node in htmlDoc.DocumentNode.QuerySelectorAll("[data-content-name]"))
+                        {
+                            // Add XHTML namespace to all elements in the content markup
+                            //
+                            foreach (var element in node.QuerySelectorAll("*"))
+                            {
+                                element.SetAttributeValue("xmlns", "http://www.w3.org/1999/xhtml");
+                            }
+
+                            var taggedContentXml = new StringBuilder();
+                            taggedContentXml.Append("<TaggedContent><name>");
+                            taggedContentXml.Append(node.Attributes["data-content-name"].Value);
+                            taggedContentXml.Append("</name><content>");
+                            taggedContentXml.Append(node.InnerHtml);
+                            taggedContentXml.Append("</content></TaggedContent>");
+                            XmlDocument xmlDoc = new XmlDocument();
+                            xmlDoc.LoadXml(taggedContentXml.ToString());
+                            ItemFields taggedContent = new ItemFields(xmlDoc.DocumentElement, taggedContentSchema);
+                            taggedContentList.Values.Add(taggedContent);
+                        }
+
+                        foreach (var node in htmlDoc.DocumentNode.QuerySelectorAll("[data-image-name]"))
+                        {
+                            Logger.Write("Processing image tag...", "CampaignZipImporter", LogCategory.Custom, System.Diagnostics.TraceEventType.Information);
+
+                            var imageUrl = node.Attributes["src"];
+                            if (imageUrl != null)
+                            {
+                                Logger.Write("Image URL:" + imageUrl.Value, "CampaignZipImporter", LogCategory.Custom, System.Diagnostics.TraceEventType.Information);
+                                ZipArchiveEntry imageEntry = archive.GetEntry(imageUrl.Value);
+                                if (imageEntry != null)
+                                {
+                                    Logger.Write("Image Length: " + imageEntry.Length, "CampaignZipImporter", LogCategory.Custom, System.Diagnostics.TraceEventType.Information);
+                              
+                                    // TODO: Store images in a sub-directory, e.g. 'images'
+
+                                    Component imageComponent = new Component(session, orgItem.Id);
+
+                                    // TODO: Have a better algorithm for the image name
+                                    var imageName = component.Title + "-" + imageUrl.Value.Replace("/", "-");
+                                    var metadataXml = new XmlDocument();
+                                    metadataXml.LoadXml(@"<Metadata xmlns=""http://www.sdl.com/web/schemas/core""/>"); // TODO: Get this namespace from the schema def
+                                    imageComponent.Schema = imageSchema;
+                                    imageComponent.Metadata = metadataXml.DocumentElement;
+                                    imageComponent.Title = imageName;
+                                    imageComponent.BinaryContent.MultimediaType = new MultimediaType(new TcmUri("tcm:0-3-65544"), session); // TEMP HARDCODE...
+                                    imageComponent.BinaryContent.UploadFromStream = imageEntry.Open();
+                                    imageComponent.BinaryContent.Filename = imageName;                   
+                                    imageComponent.Save(true);
+
+                                    // TODO: Add error handling if images already exist
+                                    var taggedImageXml = new StringBuilder();
+                                    taggedImageXml.Append("<TaggedImage xmlns:xlink=\"http://www.w3.org/1999/xlink\"><name>");
+                                    taggedImageXml.Append(node.Attributes["data-image-name"].Value);
+                                    taggedImageXml.Append("</name><image xlink:type=\"simple\" xlink:href=\"");
+                                    taggedImageXml.Append(imageComponent.Id);
+                                    taggedImageXml.Append("\" xlink:title=\"");
+                                    taggedImageXml.Append(imageComponent.Title);
+                                    taggedImageXml.Append("\" /></TaggedImage>");
+
+                                    XmlDocument xmlDoc = new XmlDocument();
+                                    xmlDoc.LoadXml(taggedImageXml.ToString());
+                                    ItemFields taggedImage = new ItemFields(xmlDoc.DocumentElement, taggedImageSchema);
+                                    taggedImageList.Values.Add(taggedImage);
+                                }
+                            }
+                        }
+
+                        foreach (var node in htmlDoc.DocumentNode.QuerySelectorAll("[data-property-name]"))
+                        {
+                            Logger.Write("Processing property tag...", "CampaignZipImporter", LogCategory.Custom, System.Diagnostics.TraceEventType.Information);
+
+                            var propertyName = node.Attributes["data-property-name"];
+                            var propertyTarget = node.Attributes["data-property-target"];
+                            if ( propertyTarget == null ) {
+                                Logger.Write("Missing property target for property '" + propertyName.Value + "'. Skpping property...", "CampaignZipImporter", LogCategory.Custom, System.Diagnostics.TraceEventType.Warning);
+                                continue;
+                            }
+                            var propertyValue = node.Attributes[propertyTarget.Value];
+
+                            var taggedPropertyXml = new StringBuilder();
+                            taggedPropertyXml.Append("<TaggedProperty xmlns:xlink=\"http://www.w3.org/1999/xlink\"><name>");
+                            taggedPropertyXml.Append(propertyName.Value);
+                            taggedPropertyXml.Append("</name><value>");
+                            taggedPropertyXml.Append(propertyValue.Value);
+                            taggedPropertyXml.Append("</value>");
+                            taggedPropertyXml.Append("<target>");
+                            taggedPropertyXml.Append(propertyTarget.Value);
+                            taggedPropertyXml.Append("</target></TaggedProperty>");
+
+                            XmlDocument xmlDoc = new XmlDocument();
+                            xmlDoc.LoadXml(taggedPropertyXml.ToString());
+                            ItemFields taggedProperty = new ItemFields(xmlDoc.DocumentElement, taggedPropertySchema);
+                            taggedPropertyList.Values.Add(taggedProperty);
+
+                        }
+
+                            component.Metadata = content.ToXml();
+                        component.Save();
                     }
-
-                    component.Metadata = content.ToXml();
-                    component.Save();
-                    File.Delete(zipFilename);
                 }
+                File.Delete(zipFilename);
             }
 
         }
