7e6df92634bddeb7883e2262843ba92e09fa7fae
Niclas Cedermalm <ncedermalm@sdl.com>
2020-10-20 13:28:01 +0200

    Updated version no for DXA DLL

--- dotnet/SDL.DXA.Modules.CampaignContent/Properties/AssemblyInfo.cs
+++ dotnet/SDL.DXA.Modules.CampaignContent/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SDL Plc")]
 [assembly: AssemblyProduct("SDL.DXA.Modules.CampaignContent")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
