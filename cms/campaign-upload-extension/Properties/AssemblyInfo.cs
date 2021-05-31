2baea1b0b0f2a2f6ab7781d3f106e522bf222e6f
Niclas Cedermalm <ncedermalm@sdl.com>
2021-05-31 14:36:10 +0200

    Upgraded 3PPs and increased version no to v1.3.1

--- cms/campaign-upload-extension/Properties/AssemblyInfo.cs
+++ cms/campaign-upload-extension/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HTML Campaign Upload Extension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SDL Plc")]
+[assembly: AssemblyCompany("RWS")]
 [assembly: AssemblyProduct("HTML Campaign Upload Extension")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
